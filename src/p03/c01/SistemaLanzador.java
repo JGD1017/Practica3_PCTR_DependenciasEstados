cd0d4b112c9804958c3bea3f4330339b16476365
JGD1017 <119081106+JGD1017@users.noreply.github.com>
2024-03-19 06:48:25 +0100

    Update SistemaLanzador.java

--- src/p03/c01/SistemaLanzador.java
+++ src/p03/c01/SistemaLanzador.java
@@ -15,12 +15,12 @@ public class SistemaLanzador {
 			// Creación de hilos de entrada
 			ActividadEntradaPuerta entradas = new ActividadEntradaPuerta(puerta, parque);
 			new Thread (entradas).start();
-			
-			// 
-			// TODO
-			//
-			
-			
+				
+			// Creación de hilos de salida
+			ActividadSalidaPuerta salidas = new ActividadSalidaPuerta(puerta, parque);
+			new Thread (salidas).start();
+				
 		}
+		
 	}	
 }
